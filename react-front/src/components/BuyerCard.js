003c67ea8436d9fec369ac7a3b517e8fdad04634
Nitya Bhanu Gogawat <nityabhanu2001@gmail.com>
2022-11-05 12:35:10 +0530

    added login page raw

---
+++ react-front/src/components/BuyerCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BuyerCard = () => {
+  return (
+    <div className='BuyerCard'>
+        <img src='#' alt=''/>
+        <h3 className='productName'>P. Name</h3>
+        <p className='prodLocation'>Location</p>
+        <p className='prodAbout'>About</p>
+        <p className='prodDuration'>Prod Duration</p>
+        <p className='prodBidtime'>Bid time here</p>
+    </div>
+  )
+}
+
+export default BuyerCard
---
+++ react-front/src/pages/login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div className='loginPage'>
+        <img src='#' alt='website logo'/>
+        <div className='siteName'>studHelpers</div>
+        <div className='SignInHeading'>Sign in or Sign Up</div>
+        <div className='loginFooter'>By Continuing, you acknowledged that you have read and understood, and agreed to studHelper's Terms of Service and Privacy Policy</div>
+    </div>
+  )
+}
+export default login
